493b03d8dc73d54c727d6e8c30208421fae5e3ab
IerominSergii <iconboooook@gmail.com>
2018-11-28 16:48:38 +0200

    Add countGamePoints and createGameTimer functions

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -1,18 +1,17 @@
-const answerPoints = {
+// count results function ---- start
+const ANSWER_POINTS = {
   correct: 100,
   fast: 150,
   slow: 50,
   false: 0
 };
-
-const savedLifeValue = 50;
-
-const maxLivesAmount = 3;
-const gameLevels = 10;
+const SAVED_LIFE_VALUE = 50;
+const MAX_LIVES_AMOUNT = 3;
+const GAME_LEVELS = 10;
 
 export const countGamePoints = (answers, savedLife) => {
   // lives
-  if (savedLife > maxLivesAmount) {
+  if (savedLife > MAX_LIVES_AMOUNT) {
     throw new Error(`Enormous lives amount, more than max lives`);
   }
 
@@ -21,7 +20,7 @@ export const countGamePoints = (answers, savedLife) => {
   }
 
   // answers
-  if (answers.length > gameLevels) {
+  if (answers.length > GAME_LEVELS) {
     throw new Error(`Enormous results amount, more than levels`);
   }
 
@@ -41,14 +40,86 @@ export const countGamePoints = (answers, savedLife) => {
       throw new Error(`Answer should be in string type`);
     }
 
-    if (isNaN(answerPoints[answer])) {
-      throw new Error(`Unknown answer type, check the 'answerPoints' object`);
+    if (isNaN(ANSWER_POINTS[answer])) {
+      throw new Error(`Unknown answer type, check the 'ANSWER_POINTS' object`);
     }
 
-    answersResult += answerPoints[answer];
+    answersResult += ANSWER_POINTS[answer];
   });
 
-  const lifeResult = savedLife * savedLifeValue;
+  const lifeResult = savedLife * SAVED_LIFE_VALUE;
 
   return answersResult + lifeResult;
 };
+// count results function ---- end
+
+// timer function ---- start
+const MAX_TIMER_VALUE = 30;
+
+export const createGameTimer = (time) => {
+  if (time <= 0) {
+    throw new Error(`Time shouldn't be negative or equal 0`);
+  }
+
+  if (time > MAX_TIMER_VALUE) {
+    throw new Error(`Time shouldn't be more than MAX_TIMER_VALUE`);
+  }
+
+  const gameTimer = {
+    time,
+    tick() {
+      this.time = this.time -= 1;
+      if (this.time === 0) {
+        return `Time is over`;
+      }
+
+      return this.time;
+    }
+  };
+
+  return gameTimer;
+};
+// timer function ---- end
+
+// goNextLevel function ---- start
+
+// ---- STATE EXAMPLE ----
+// const state = {
+//   currentLevel: 0,
+//   answers: [],
+//   isFinished: false
+// };
+const setAnswerDependsOnTheTime = (time, answers) => {
+  const newAnswers = answers.slice();
+
+  if (time <= 30 && time > 20) {
+    newAnswers.push(`fast`);
+  } else if (time <= 20 && time > 10) {
+    newAnswers.push(`correct`);
+  } else if (time <= 10 && time > 0) {
+    newAnswers.push(`slow`);
+  } else {
+    newAnswers.push(`wrong`);
+  }
+
+  return newAnswers;
+};
+
+export const goNextLevel = (state, time) => {
+  const {answers, currentLevel, isFinished} = state;
+
+  if (currentLevel < 10) {
+    answers = setAnswerDependsOnTheTime(time, answers);
+  } else {
+    isFinished = true;
+  }
+
+  const newState = Object.assign({}, state, {
+    answers,
+    currentLevel,
+    isFinished
+  });
+
+  return newState;
+};
+// goNextLevel function ---- end
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-nested-callbacks */
 import {assert} from "chai";
-import {countGamePoints} from "./game-data";
+import {countGamePoints, createGameTimer, goNextLevel} from "./game-data";
 
 describe(`CountGamePoints function:`, () => {
   describe(`correct results:`, () => {
@@ -78,7 +78,7 @@ describe(`CountGamePoints function:`, () => {
       ];
       assert.throws(() => {
         countGamePoints(answers, 0);
-      }, /Unknown answer type, check the 'answerPoints' object/);
+      }, /Unknown answer type, check the 'ANSWER_POINTS' object/);
     });
     it(`shouldn't work with number type results`, () => {
       const answers = [
@@ -174,6 +174,90 @@ describe(`CountGamePoints function:`, () => {
       assert.throws(() => {
         countGamePoints(answers, 0);
       }, /Answers array shouldn't be empty/);
+    });
+  });
+});
+
+describe(`Timer`, () => {
+  describe(`correct results:`, () => {
+    it(`should return the timer object`, () => {
+      const gameTimer = {
+        time: 30,
+        tick() {
+          this.time = this.time -= 1;
+          if (this.time === 0) {
+            return `Time is over`;
+          }
+
+          return this.time;
+        }
+      };
+      assert.equal(createGameTimer(30), gameTimer);
+    });
+    it(`should decrease the time`, () => {
+      const gameTimer = createGameTimer(30);
+      gameTimer.tick();
+      assert.equal(gameTimer.time, 29);
+    });
+    it(`should report the time is over`, () => {
+      const gameTimer = createGameTimer(1);
+      const timerMessage = gameTimer.tick();
+      assert.equal(timerMessage, `Time is over`);
+    });
+  });
+  describe(`incorrect results:`, () => {
+    it(`shouldn't work with negative time value or 0`, () => {
+      assert.throws(() => {
+        createGameTimer(-1);
+      }, /Time shouldn't be negative or equal 0/);
+    });
+    it(`shouldn't work with more than MAX_TIMER_VALUE`, () => {
+      assert.throws(() => {
+        createGameTimer(31);
+      }, /Time shouldn't be more than MAX_TIMER_VALUE/);
+    });
+  });
+});
+
+describe(`goNextLevel`, () => {
+  describe(`correct results:`, () => {
+    it(`should add new results`, () => {
+      const timeLeft = 15;
+      const newAnswers = goNextLevel([`fast`, `slow`], timeLeft);
+      assert.deepEqual(newAnswers, [`fast`, `slow`, `correct`]);
+    });
+    it(`should count fast answer properly`, () => {
+      const timeLeft = 25;
+      const state = {
+        currentLevel: 0,
+        answers: [`fast`, `slow`],
+        isFinished: false
+      };
+      const newAnswers = goNextLevel(state, timeLeft);
+      assert.deepEqual(newAnswers.answers, [`fast`, `slow`, `fast`]);
+    });
+    it(`should count slow answer properly`, () => {
+      const timeLeft = 5;
+      const newAnswers = goNextLevel([`fast`, `slow`], timeLeft);
+      assert.deepEqual(newAnswers, [`fast`, `slow`, `slow`]);
+    });
+    it(`should add wrong result if time was over`, () => {
+      const timeLeft = 0;
+      const newAnswers = goNextLevel([`fast`, `slow`], timeLeft);
+      assert.deepEqual(newAnswers, [`fast`, `slow`, `wrong`]);
+    });
+  });
+
+  describe(`incorrect results:`, () => {
+    it(`shouldn't go to the next level`, () => {
+      assert.throws(() => {
+        createGameTimer(-1);
+      }, /Time shouldn't be negative or equal 0/);
+    });
+    it(`shouldn't work with more than MAX_TIMER_VALUE`, () => {
+      assert.throws(() => {
+        createGameTimer(31);
+      }, /Time shouldn't be more than MAX_TIMER_VALUE/);
     });
   });
 });
