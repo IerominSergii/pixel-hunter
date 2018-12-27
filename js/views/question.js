9dcd5ee2260dc65024900047c8418b64b10ab68b
IerominSergii <iconboooook@gmail.com>
2018-12-27 22:50:13 +0200

    Simplify getQuestionTemplate function

--- js/views/question.js
+++ js/views/question.js
@@ -1,35 +1,53 @@
-import {questionsType} from "./../game/configuration";
+// import {questionsType} from "./../game/configuration";
 
 import questionSingle from "./../templates/question-single";
 import questionTwice from "./../templates/question-twice";
 import questionTriple from "./../templates/question-triple";
 
 import currentStats from "./../templates/current-stats";
+// import {createElement} from "../util/util";
+
+const templates = {
+  single: questionSingle,
+  twice: questionTwice,
+  triple: questionTriple
+};
 
 const getQuestionTemplate = (question) => {
-  switch (question.type) {
-    case questionsType.SINGLE: {
-      return questionSingle(question);
-    }
-    case questionsType.TWICE: {
-      return questionTwice(question);
-    }
-    case questionsType.TRIPLE: {
-      return questionTriple(question);
-    }
-    default: {
-      throw new Error(`Wrong question type in question.js`);
-    }
-  }
+  const template = templates[question.type](question);
+  return template;
 };
+
+// const bindHandler = (type, element, callback, timerId) => {
+//   const handler = (evt) => {
+//     clearInterval(timerId);
+//     callback(evt);
+//   };
+
+//   const gameOption = element.querySelector(`.game__content`);
+//   if (type === questionsType.TRIPLE) {
+//     gameOption.addEventListener(`click`, handler);
+//   } else {
+//     gameOption.addEventListener(`change`, handler);
+//   }
+// };
 
 const questionView = (state) => {
   const currentQuestion = state.questions[state.currentQuestion];
 
+  //   const questionTemplate = `<section class="game">
+  //   ${getQuestionTemplate(currentQuestion)}
+  //   ${currentStats(state.answers, state.questions.length)}
+  // </section>`;
+
+  // const questionElement = createElement(questionTemplate);
+  // bindHandler();
+
+  // return questionElement;
   return `<section class="game">
-    ${getQuestionTemplate(currentQuestion)}
-    ${currentStats(state.answers, state.questions.length)}
-  </section>`;
+  ${getQuestionTemplate(currentQuestion)}
+  ${currentStats(state.answers, state.questions.length)}
+</section>`;
 };
 
 export default questionView;
