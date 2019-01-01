6a65b4ed1fc3b63215cb89c51ee8af39e28f35e0
IerominSergii <iconboooook@gmail.com>
2019-01-01 13:59:43 +0200

    Move isAnswerCorrect into the questions

--- js/views/question.js
+++ js/views/question.js
@@ -6,61 +6,64 @@ import questionTriple from "./../templates/question-triple";
 
 import currentStats from "./../templates/current-stats";
 import {createElement} from "../util/util";
-import isAnswerCorrect from "../game/is-answer-correct";
 
-const templates = new Map([
-  [questionsType.SINGLE, questionSingle],
-  [questionsType.TWICE, questionTwice],
-  [questionsType.TRIPLE, questionTriple]
-]);
-
-const eventNames = new Map([
-  [questionsType.SINGLE, `change`],
-  [questionsType.TWICE, `change`],
-  [questionsType.TRIPLE, `click`]
-]);
-
-const getQuestionTemplate = (question) => {
-  return templates.get(question.type)(question);
+const templates = {
+  single: questionSingle,
+  twice: questionTwice,
+  triple: questionTriple
 };
 
-const takeUserAnswers = (evt, type) => {
+const eventNames = {
+  single: `change`,
+  twice: `change`,
+  triple: `click`
+};
+
+const isAnswerCorrect = (question, evt) => {
+  const {type, options} = question;
+
   switch (type) {
-    case `single`:
-      return document.querySelector(`input:checked`).value;
-    case `twice`:
+    case questionsType.SINGLE:
+      const userAnswer = document.querySelector(`input:checked`).value;
+      const singleOptionIndex = 0;
+      return userAnswer === options[singleOptionIndex].thisIs;
+    case questionsType.TWICE:
       const totalQuestionsLength = document.querySelectorAll(`.game__option`)
         .length;
       const checkedInputs = document.querySelectorAll(`input:checked`);
-      const values = Array.from(checkedInputs).map((input) => {
+      const answerValues = Array.from(checkedInputs).map((input) => {
         return input.value;
       });
-      return {totalQuestionsLength, values};
-    case `triple`:
-      const userAnswer = evt.target.getAttribute(`data-answer`);
-      return userAnswer;
-    default:
-      throw new Error(
-          `Wrong answer type was given to the takeUserAnswers function`
-      );
+
+      if (answerValues.length === totalQuestionsLength) {
+        const initialResult = false;
+
+        return answerValues.reduce((result, answer, index) => {
+          return result && answer === options[index];
+        }, initialResult);
+      } else {
+        return `answerWasNotTaken`;
+      }
+    case questionsType.TRIPLE:
+      return evt.target.getAttribute(`data-answer`) === `paint`;
+    default: {
+      throw new Error(`Wrong question type`);
+    }
   }
 };
 
 const questionView = (state, callback, timerId) => {
   const currentQuestion = state.questions[state.currentQuestion];
-  const eventName = eventNames.get(currentQuestion.type);
+  const {type} = currentQuestion;
+  const eventName = eventNames[type];
 
   const questionTemplate = `<section class="game">
-    ${getQuestionTemplate(currentQuestion)}
+    ${templates[type](currentQuestion)}
     ${currentStats(state.answers, state.questions.length)}
   </section>`;
 
   const handler = (evt) => {
-    const userAnswers = takeUserAnswers(evt, currentQuestion.type);
-    const isCurrentAnswerCorrect = isAnswerCorrect(
-        currentQuestion,
-        userAnswers
-    );
+    const isCurrentAnswerCorrect = isAnswerCorrect(currentQuestion, evt);
 
     if (isCurrentAnswerCorrect !== `answerWasNotTaken`) {
       clearInterval(timerId);
